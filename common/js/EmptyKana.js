b4bd191b75a57936f1479d0b31553f9b4769305b
lexamo <lexamo.liu@gmail.com>
2017-08-17 17:55:38 +0800

    refactiring, remove long method in words.js

---
+++ common/js/EmptyKana.js
@@ -0,0 +1,20 @@
+function EmptyKana() {
+    EmptyKana._super.call(this);
+    this.word = ".";
+    this.spell = ".";
+};
+
+inherits(EmptyKana, Kana);
+
+EmptyKana.prototype.GetTypeName = function() {
+    return this.constructor.name;
+};
+
+function checkObjectIsEmptyKana(kana) {
+    if (typeof(kana.GetTypeName) === 'function') {
+        if (kana.GetTypeName() == "EmptyKana")
+            return true;
+    }
+    logging("Object is not a EmptyKana: " + kana);
+    return false;
+};
---
+++ common/js/KanaTable.js
@@ -0,0 +1,9 @@
+var KanaTable = function KanaTable() {
+    this.kanaRowList = {};
+};
+
+KanaTable.prototype.AddKanaRow = function(kanaRow) {
+    if (checkObjectIsKanaRow(kanaRow)) {
+        this.kanaRowList[kanaRow.keySpell] = kanaRow;
+    }
+};
